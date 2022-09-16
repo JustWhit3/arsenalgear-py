91127173d265ae615692d8b83c1050a068112e81
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2022-09-17 00:57:23 +0200

    setup.py file update.

--- src/setup.py
+++ src/setup.py
@@ -15,19 +15,21 @@ from setuptools import find_packages, setup
 setup(
     name = "arsenalgear",
     packages = find_packages( include = [ "arsenalgear" ] ),
-    version = "1.2.2",
+    version = "1.3.0",
     description = "A library containing general purpose Python utils",
     author = "Gianluca Bianco",
     author_email = 'biancogianluca9@gmail.com',
     url = 'https://justwhit3.github.io/', 
-    download_url = 'https://github.com/JustWhit3/arsenalgear/archive/refs/tags/v1.2.2.zip',
+    download_url = 'https://github.com/JustWhit3/arsenalgear-py/archive/refs/tags/v1.3.0.zip',
     license = "MIT",
     install_requires = [ 
-                        "matplotlib>=3.1.2", 
-                        "numpy>=1.17.4", 
-                        "scipy==1.6.2",
+                        "matplotlib==3.5.1",
+                        "numpy==1.21.5",
+                        "scikit_learn==1.1.2",
+                        "scipy==1.8.0",
+                        "setuptools==59.6.0",
                         "termcolor==1.1.0",
-                        "tqdm>=4.64.0"
+                        "tqdm==4.64.0"
                        ],
     classifiers=[
       'Development Status :: 4 - Beta',
@@ -35,6 +37,6 @@ setup(
       'Topic :: Software Development',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.10',
     ]
 )
